cd136eadb1465a9e4dd944dbebfb1617d7e78d12
Gastardeli <victorgastardeli11@gmail.com>
2026-03-24 21:39:06 -0300

    V1 do novo login + inserção de template modal

---
+++ public/js/modal.js
@@ -0,0 +1,19 @@
+
+function loadingModal() {
+    const barra = document.getElementById("preenchimento");
+    const modal = document.getElementById("modal");
+    let progresso = 0;
+    const timer = setInterval(() => {
+        progresso++;
+        barra.style.width = progresso + "%";
+
+        if (progresso >= 100) {
+            clearInterval(timer);
+            modal.style.display = "none";
+        }
+    }, 30);
+}
+
+
+
+
