99c4cddf849651417486fffdda02c977cb2303b1
GabrielMarRib <gabriel.mribeiro@sptech.school>
2026-04-03 17:42:21 -0300

    dois graficos montados

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(emailVar, senhaVar) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", emailVar, senhaVar)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email, fkGerente FROM usuario WHERE email = '${emailVar}' AND senha = '${senhaVar}';
+        SELECT idUsuario, nome, email, fkPapel FROM usuario WHERE email = '${emailVar}' AND senha = '${senhaVar}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -13,7 +13,7 @@ function autenticar(emailVar, senhaVar) {
 function cadastrar(nome, email, senha, cpf, idGerente) {
 
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, cpf, senha, fkGerente)
+        INSERT INTO usuario (nome, email, cpf, senha, fkPapel)
         VALUES ('${nome}', '${email}', '${cpf}', '${senha}', '${idGerente}');
     `;
     // var instrucaoSql = `
