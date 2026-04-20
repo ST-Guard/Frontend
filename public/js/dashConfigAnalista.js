954b76468fb478ab2bc5de0c40067f8c71335aee
smg1708 <samuelssousaky@gmail.com>
2026-04-20 18:43:41 -0300

    correcaoMascara

---
+++ public/js/dashConfigAnalista.js
@@ -0,0 +1,15 @@
+function mascaraTel(i) {
+    var celular = i.value.replace(/\D/g, "");
+
+    if (celular.length > 11) celular = celular.slice(0, 11);
+
+    if (celular.length > 0) {
+        celular = celular.replace(/^(\d{2})(\d)/g, "($1) $2");
+    }
+
+    if (celular.length > 10) {
+        celular = celular.replace(/(\d{5})(\d)/, "$1-$2");
+    }
+
+    i.value = celular;
+}
--- public/js/dashConfigGestor.js
+++ public/js/dashConfigGestor.js
@@ -1,17 +1,15 @@
 function mascaraTel(i) {
-    let v = i.value.replace(/\D/g, "");
+    var celular = i.value.replace(/\D/g, "");
 
-    if (v.length > 11) v = v.slice(0, 11);
+    if (celular.length > 11) celular = celular.slice(0, 11);
 
-    if (v.length > 0) {
-        v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
+    if (celular.length > 0) {
+        celular = celular.replace(/^(\d{2})(\d)/g, "($1) $2");
     }
 
-    if (v.length > 10) {
-        v = v.replace(/(\d{5})(\d)/, "$1-$2");
-    } else {
-        v = v.replace(/(\d{4})(\d)/, "$1-$2");
+    if (celular.length > 10) {
+        celular = celular.replace(/(\d{5})(\d)/, "$1-$2");
     }
 
-    i.value = v;
+    i.value = celular;
 }
