e3f7b9d4621ab6bcdac547482213bc4324fbf3cb
Gastardeli <victorgastardeli11@gmail.com>
2026-04-30 08:29:13 -0300

    values campo de cadastro

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,7 +1,5 @@
 show databases;
-
-drop database smartData;
-
+DROP DATABASE smartdata;
 CREATE DATABASE smartData;
 USE smartData;
 
@@ -26,9 +24,12 @@ fkEmpresa INT,
 CREATE TABLE usuario (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100),
+imagem VARCHAR(255),
 email VARCHAR(200),
 cpf CHAR(11),
+telefone CHAR(15),
 senha VARCHAR(50),
+status VARCHAR(20) DEFAULT 'Ativo',
 fkPapel INT,
 	CONSTRAINT fkPapel
     FOREIGN KEY(fkPapel)
@@ -47,15 +48,11 @@ fkUsuarioDataCenter INT,
 
 CREATE TABLE zona (
 idZona INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(45),
+nome ENUM('Zona A', 'Zona B', 'Zona C') NOT NULL,
 fkDataCenter INT,
 	CONSTRAINT fkDataCenter
     FOREIGN KEY(fkDataCenter)
-	REFERENCES datacenter(idDataCenter),
-fkUsuarioZona INT,
-	CONSTRAINT fkUsuarioZona
-    FOREIGN KEY(fkUsuarioZona)
-    REFERENCES usuario(idUsuario)
+	REFERENCES datacenter(idDataCenter)
 );
 
 CREATE TABLE regiao(
@@ -63,6 +60,7 @@ idRegiao INT PRIMARY KEY AUTO_INCREMENT,
 cep CHAR(8),
 numero VARCHAR(45),
 complemento VARCHAR(45),
+estado CHAR(2),
 fkRegiaoEmpresa INT,
 	CONSTRAINT fkRegiaoEmpresa
     FOREIGN KEY(fkRegiaoEmpresa)
@@ -109,9 +107,8 @@ fkComponentes INT,
 
 CREATE TABLE registro(
 idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-cep CHAR(8),
-numero VARCHAR(45),
-complemento VARCHAR(45),
+dataHora DATETIME,
+valor FLOAT,
 fkRegistroServidor INT,
 	CONSTRAINT fkRegistroServidor
     FOREIGN KEY(fkRegistroServidor)
@@ -122,38 +119,90 @@ fkRegistroComponente INT,
     REFERENCES componentes(idComponente)
 );
 
+CREATE TABLE contato_inicial(
+idContato_inicial INT PRIMARY KEY AUTO_INCREMENT,
+nome_usuario VARCHAR(45),
+email_usuario VARCHAR(45),
+mensagem_usuario VARCHAR(45)
+);
+
+ALTER TABLE usuario
+ADD COLUMN fkZona INT,
+ADD CONSTRAINT fkUsuarioZona
+FOREIGN KEY (fkZona) REFERENCES zona(idZona);
+
 INSERT INTO empresa (razaoSocial, cnpj, telefoneEmpresa, tokenEmpresa) VALUES
-	('Steam', '12345678910119 ', '11123456789', 'STE12345');
+	('Steam', '12345678910119', '11123456789', 'STE12345');
     
 INSERT INTO papel (nivel, descricao, fkEmpresa) VALUES
 	('Gestor', 'Acesso total ao sistema', 1),
 	('Analista', 'Monitoramento de servidores', 1);
-    
-INSERT INTO usuario (nome, email, cpf, senha, fkPapel) VALUES
-	('Carlos Gestor', 'carloss@gmail.com', '12345678910', '123456', 1),
-	('Ana Analista', 'anaa@gmail.com', '10987654321', '123456', 2);
+
+
+INSERT INTO usuario (nome, email, cpf, telefone, senha, status, fkPapel, fkZona) VALUES
+('Maria Gestora', 'maria@gmail.com', '12345678910','(11) 9999-8888', '123456', 'Ativo', 1, NULL);
 
 INSERT INTO datacenter (nome, capacidadeServidores, fkUsuarioDataCenter) VALUES
-	('ST-SP-01', 100, 1);
+('ST-SP-01', 100, 1);
+
+INSERT INTO zona (nome, fkDataCenter) VALUES
+('Zona A', 1),
+('Zona B', 1),
+('Zona C', 1);
+
+INSERT INTO usuario (nome, email, cpf, telefone, senha, status, fkPapel, fkZona) VALUES
+('Erick Analista', 'erick@gmail.com', '10987654321', '(11) 9999-7777', '123456', 'Ativo', 2, 1),
+('Miguel Analista', 'miguel@gmail.com', '14985559347', '(11) 9999-6666', '123456', 'Ativo', 2, 1);
     
-INSERT INTO zona (nome, fkDataCenter, fkUsuarioZona) VALUES
-	('Zona A', 1, 1);
-    
-INSERT INTO regiao (cep, numero, complemento, fkRegiaoEmpresa, fkRegiaoDataCenter) VALUES
-	('12345678', '9101', 'Steam Sp', 1, 1);
+INSERT INTO regiao (cep, numero, complemento, estado, fkRegiaoEmpresa, fkRegiaoDataCenter) VALUES
+	('12345678', '9101', 'Steam Sp', 'SP', 1, 1);
     
 INSERT INTO servidor (nome, tipo, estado, fkZona) VALUES
-	('SERVIDOR-DC01-WEB-05', 'Web Server', 'Ativo' , 1);
+	('SRV-DC01-WEB-05', 'Web', 'Ativo', 1),
+    ('SRV-DC01-DB-12', 'DB', 'Ativo', 1),
+    ('SRV-FK02-GM-02', 'GM', 'Ativo', 1),
+    ('SRV-DC01-WEB-08', 'WEB', 'Ativo', 1);
     
 INSERT INTO componentes (nome, tipo, unidadeMedida, capacidadeMaxima) VALUES
 	('CPU', 'Processador', '%', 100),
 	('RAM', 'Memoria', 'GB', 20),
-	('DISCO', 'Armazenamento', 'GB', 512);
+	('DISCO', 'Armazenamento', 'GB', 512),
+    ('REDE', 'LATENCIA', 'MBps', 50);
     
 INSERT INTO componentes_servidor (limite, fkServidor, fkComponentes) VALUES
 	(90, 1, 1),
 	(16, 1, 2),
-	(450, 1, 3);
+	(450, 1, 3),
+	(40, 1, 4);
     
-INSERT INTO registro (cep, numero, complemento, fkRegistroServidor, fkRegistroComponente) VALUES
-('12345678', '9101', 'Deck 01', 1, 1);
+INSERT INTO registro (dataHora, valor, fkRegistroServidor, fkRegistroComponente) VALUES
+    ('2025-08-19 10:00:00', 52, 1, 1),
+    ('2025-08-19 10:00:00', 6.8, 1, 2),
+    ('2025-08-19 10:00:00', 238, 1, 3);
+    
+create view vwBuscarDados AS
+	select u.idUsuario,
+		u.nome as nomePessoa,
+		u.imagem,
+		u.telefone,
+		u.email,
+		u.cpf,
+		p.nivel as cargo,
+		p.descricao as bio,
+		d.nome as nomeDataCenter,
+		z.nome as nomeZona
+        from usuario u
+        join papel p
+			on  p.idPapel = u.fkPapel
+		left join zona z
+			on z.idZona = u.fkZona
+		LEFT JOIN datacenter d
+			ON (
+				(p.nivel = 'Analista' AND d.idDataCenter = z.fkDataCenter)
+				OR
+				(p.nivel = 'Gestor' AND d.fkUsuarioDataCenter = u.idUsuario)
+			);
+            
+select * from contato_inicial;
+
+select * from vwBuscarDados
