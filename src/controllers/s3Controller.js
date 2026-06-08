a642e97b3ddce3ee837df5ccd7e713cf53eb0727
GabrielMarRib <gabriel.mribeiro@sptech.school>
2026-06-07 22:40:12 -0300

    fix: caminho json

--- src/controllers/s3Controller.js
+++ src/controllers/s3Controller.js
@@ -15,7 +15,7 @@ async function obterS3UrlController(req, res) {
     try {
         const comando = new GetObjectCommand({
             Bucket: process.env.AWS_BUCKET_NAME,
-            Key: 'client/historico.json',
+            Key: 'client/alertas_gestora.json',
         });
 
         const url = await getSignedUrl(s3Client, comando, { expiresIn: 300 });
